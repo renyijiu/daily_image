e6a1adf83f597cacc1f5e79a434b73a593dde7ae
renyijiu <lanyuejin1108@gmail.com>
2018-09-18 22:03:56 +0800

    [test]add test code

--- test/daily_image_test.rb
+++ test/daily_image_test.rb
@@ -1,11 +1,44 @@
 require "test_helper"
 
 class DailyImageTest < Minitest::Test
-  def test_that_it_has_a_version_number
+
+  def test_should_has_version
     refute_nil ::DailyImage::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_should_get_config
+    config = DailyImage.config
+
+    assert config[:bg_color]
+    assert config[:frame_color]
+    assert config[:text_color]
+    assert config[:date_color]
+    assert config[:unused_color]
+    assert config[:used_color]
+    assert config[:font]
+    assert config[:out_frame_offset]
+    assert config[:in_frame_offset]
+  end
+
+  def test_should_set_config
+    Singleton.__init__(DailyImage::Config)
+    color = [rand(0..255), rand(0..255), rand(0..255)]
+
+    DailyImage.configure do |config|
+      config.bg_color = color
+    end
+
+    config = DailyImage.config
+    assert_equal color, config[:bg_color]
+  end
+
+  def test_should_generate_image
+    filename = "daily_#{Date.today}.jpeg"
+    file_path = File.join('./tmp/', filename)
+
+    DailyImage.draw_image('./tmp/')
+
+    assert File.file?(file_path)
+    File.delete(file_path)
   end
 end
